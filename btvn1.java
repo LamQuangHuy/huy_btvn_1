358196c22deeae8669daed728edeefa101b0e35e
Lam Quang Huy <lanquanghuy593@Lans-MacBook-Pro.local>
2023-02-24 20:10:35 +0700

    show elm

---
+++ btvn1.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class btvn1{
+    // public static int sum(int[]a){
+    //     int sum=0;
+    //     for(int s:a ){
+    //         sum+=s;
+    //     }
+    //     return sum;
+    // }
+    public static void main(String[] args) {
+        int a[]={2,6,4,1,9};
+        // System.out.println(sum(a));
+        System.out.println(Arrays.toString(a));
+
+    }
+}
